aa07cc851fe87dcb3c7f253ed44676f01cc252bc
Laura <Laurxding@hotmail.com>
2021-04-30 14:40:10 +0200

    cart fix

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,25 +1,46 @@
-import { IonContent, IonPage, IonItem, IonLabel, IonHeader, IonTitle, IonToolbar, IonCol } from "@ionic/react";
+import {
+  IonContent,
+  IonPage,
+  IonItem,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonCol,
+  IonIcon,
+} from "@ionic/react";
 import CartContext from "../contexts/CartContext";
+import { closeOutline } from "ionicons/icons";
 import styles from "./Cart.module.css";
 import React, { useContext, useState, useEffect } from "react";
 import TotalToPay from "../components/TotalToPay";
 
 const Cart: React.FC = () => {
-  const { books, updateBooks } = useContext(CartContext);
+  const { books, setBooks } = useContext(CartContext);
 
   let countTotal: number = 0;
   const booklist = books.map((book: any) => {
     const price: number = Number(book.prix);
     countTotal = countTotal + price;
+    const removeItem = () => {
+      const i = books.indexOf(book);
+      const newArrayBook = [...books];
+      newArrayBook.splice(i, 1);
+      setBooks(newArrayBook);
+    };
 
     return (
       <IonItem key={book.titre}>
+        <IonIcon
+          onClick={removeItem}
+          style={{ fontSize: "20px" }}
+          icon={closeOutline}
+        ></IonIcon>
         <IonCol size="5">
-          <img className={styles['img']} src={book.image}></img>
+          <img className={styles["img"]} src={book.image}></img>
         </IonCol>
         <p>
           {book.titre} {book.auteur}
-          <p className={styles['price']}>Prix: {book.prix} €</p>
+          <p className={styles["price"]}>Prix: {book.prix} €</p>
         </p>
       </IonItem>
     );
@@ -38,8 +59,8 @@ const Cart: React.FC = () => {
         <div className="background">
           <p className={styles["title-tab-2"]}>PANIER</p>
           {booklist}
-          <div className={styles['pay']}>
-            <TotalToPay total={countTotal} />
+          <div className={styles["pay"]}>
+              <TotalToPay total={countTotal} />
           </div>
         </div>
       </IonContent>
